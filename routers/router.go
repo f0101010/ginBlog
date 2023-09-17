e75fed3b36cbb0169d47589913039c61f2c1aeaf
Elmer <fbl010316@gmail.com>
2023-09-17 18:18:53 +0800

    错误处理模块+用户路由

--- routers/router.go
+++ routers/router.go
@@ -1,22 +1,25 @@
 package routers
 
 import (
+	v1 "ginBlog/api/v1"
 	"ginBlog/utils"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func InitRouter() {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
 
-	router := r.Group("api/v1")
+	routerV1 := r.Group("api/v1")
 	{
-		router.GET("/hello", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "hello world!",
-			})
-		})
+		// User module routing
+		routerV1.POST("user/add", v1.AddUser)
+		routerV1.GET("users", v1.GetUsers)
+		routerV1.PUT("user/:id", v1.EditUser)
+		routerV1.DELETE("user/:id", v1.DeleteUser)
+		// Category module routing
+
+		// Article module routing
 	}
 	r.Run(utils.HttpPort)
 }
