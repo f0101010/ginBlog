0f239f0f0b9d4177bd72a727a0a7cd31cbfa24e6
Elmer <fbl010316@gmail.com>
2023-09-18 18:46:36 +0800

    博客文章接口（一）

--- model/Article.go
+++ model/Article.go
@@ -1,6 +1,10 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"ginBlog/utils/errmsg"
+	"gorm.io/gorm"
+)
 
 type Article struct {
 	Category Category `gorm:"foreignKey:Cid"`
@@ -10,4 +14,54 @@ type Article struct {
 	Desc    string `gorm:"type:varchar(200);" json:"desc"`
 	Content string `gorm:"type:longtext;" json:"content"`
 	Img     string `gorm:"type:varchar(100);" json:"img"`
+}
+
+// CreateArticle 添加文章
+func CreateArticle(data *Article) int {
+	err := db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR // 500
+	}
+	return errmsg.SUCCESS // 200
+}
+
+// todo 查询分类下的所有文章
+
+// todo 查询单个文章
+
+// GetArticle todo 查询文章列表
+func GetArticle(pageSize int, pageNum int) []Category {
+	var cate []Category
+	err = db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&cate).Error
+	if err != nil && !errors.Is(gorm.ErrRecordNotFound, err) {
+		return nil
+	}
+	return cate
+}
+
+// EditArticle 编辑文章
+func EditArticle(id int, data *Article) int {
+	var art Article
+	var maps = make(map[string]interface{})
+	maps["title"] = data.Title
+	maps["cid"] = data.Cid
+	maps["desc"] = data.Desc
+	maps["content"] = data.Content
+	maps["img"] = data.Img
+	err = db.Model(&art).Where("id = ?", id).Updates(maps).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+
+	return errmsg.SUCCESS
+}
+
+// DeleteArticle 删除文章
+func DeleteArticle(id int) int {
+	var art Article
+	err = db.Where("id = ?", id).Delete(&art).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
 }
--- routers/router.go
+++ routers/router.go
@@ -17,12 +17,18 @@ func InitRouter() {
 		routerV1.GET("users", v1.GetUsers)
 		routerV1.PUT("user/:id", v1.EditUser)
 		routerV1.DELETE("user/:id", v1.DeleteUser)
+
 		// Category module routing
 		routerV1.POST("category/add", v1.AddCategory)
 		routerV1.GET("category", v1.GetCategory)
 		routerV1.PUT("category/:id", v1.EditCategory)
 		routerV1.DELETE("category/:id", v1.DeleteCategory)
+
 		// Article module routing
+		routerV1.POST("article/add", v1.AddArticle)
+		routerV1.GET("article", v1.GetArticle)
+		routerV1.PUT("article/:id", v1.EditArticle)
+		routerV1.DELETE("article/:id", v1.DeleteArticle)
 	}
 	r.Run(utils.HttpPort)
 }
