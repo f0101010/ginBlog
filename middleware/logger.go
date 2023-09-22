7fafaea51dc6ab1433538ebc4aa869e10b5bc277
Elmer <fbl010316@gmail.com>
2023-09-22 09:34:02 +0800

    日志生成及日志分割

---
+++ middleware/logger.go
@@ -0,0 +1,86 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
+	"math"
+	"os"
+	"time"
+)
+
+func Logger() gin.HandlerFunc {
+	filePath := "log/ginBlog"
+	linkName := "latest_log.log"
+	src, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+	logger := logrus.New()
+	logger.Out = src
+
+	logger.SetLevel(logrus.DebugLevel)
+	logWriter, _ := rotatelogs.New(
+		filePath+"%Y%m%d.log",
+		rotatelogs.WithMaxAge(7*24*time.Hour),
+		rotatelogs.WithRotationTime(24*time.Hour),
+		rotatelogs.WithLinkName(linkName),
+	)
+	writeMap := lfshook.WriterMap{
+		logrus.InfoLevel:  logWriter,
+		logrus.FatalLevel: logWriter,
+		logrus.DebugLevel: logWriter,
+		logrus.WarnLevel:  logWriter,
+		logrus.ErrorLevel: logWriter,
+		logrus.PanicLevel: logWriter,
+	}
+
+	hook := lfshook.NewHook(writeMap, &logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+
+	logger.AddHook(hook)
+
+	return func(c *gin.Context) {
+		startTime := time.Now()
+		c.Next()
+		stopTime := time.Since(startTime)
+		spendTime := fmt.Sprintf("%d ms", int(math.Ceil(float64(stopTime.Nanoseconds())/1000000.0)))
+		hostName, err := os.Hostname()
+		if err != nil {
+			hostName = "unknown"
+		}
+		statusCode := c.Writer.Status()
+		clientIP := c.ClientIP()
+		userAgent := c.Request.UserAgent()
+		dataSize := c.Writer.Size()
+		if dataSize < 0 {
+			dataSize = 0
+		}
+		method := c.Request.Method
+		path := c.Request.RequestURI
+
+		entry := logger.WithFields(logrus.Fields{
+			"Hostname":  hostName,
+			"Status":    statusCode,
+			"SpendTime": spendTime,
+			"IP":        clientIP,
+			"Method":    method,
+			"Path":      path,
+			"DataSize":  dataSize,
+			"Agent":     userAgent,
+		})
+		if len(c.Errors) > 0 {
+			entry.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
+		}
+		if statusCode >= 500 {
+			entry.Error()
+		} else if statusCode >= 400 {
+			entry.Warn()
+		} else {
+			entry.Info()
+		}
+	}
+}
